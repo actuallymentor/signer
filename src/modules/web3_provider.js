ded802b7af2515ec86e93f0bc1babd22bf4b4eb5
Mentor <mentor@palokaj.co>
2022-10-28 15:44:39 +0200

    ✨ integrate rainbow, wagmi and bump dependencies

---
+++ src/modules/web3_provider.js
@@ -0,0 +1,54 @@
+// Rainbow components
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+
+// WAGMI components
+import { WagmiConfig, createClient, chain, configureChains } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { log } from './helpers'
+
+const { name } = require( '../../package.json' )
+
+/* ///////////////////////////////
+// Chain configuration
+// /////////////////////////////*/
+const { REACT_APP_alchemy_api_key } = process.env
+
+// Select chains 
+const enabled_chains = [ chain.mainnet, chain.goerli ]
+
+// Cnfigure connectors 
+const enabled_providers = [
+
+    // Backup provider to the built-in one
+    ...( REACT_APP_alchemy_api_key ? [ alchemyProvider( { apiKey: REACT_APP_alchemy_api_key } ) ] : [] ),
+
+    // The provider already exposed in this user's browser
+    publicProvider()
+
+]
+
+log( `Set up provider with ${ enabled_chains.length } chains and ${ enabled_providers.length } providers` )
+const { chains, provider } = configureChains( enabled_chains, enabled_providers )
+
+/* ///////////////////////////////
+// Rainbow configuration
+// /////////////////////////////*/
+const { connectors } = getDefaultWallets( {
+    appName: name,
+    chains
+} )
+
+/* ///////////////////////////////
+// Wagmi client
+// /////////////////////////////*/
+const wagmiClient = createClient( {
+    autoConnect: false,
+    provider,
+    connectors
+} )
+
+export default props => <WagmiConfig client={ wagmiClient }>
+    <RainbowKitProvider coolMode chains={chains} { ...props } />
+</WagmiConfig>
