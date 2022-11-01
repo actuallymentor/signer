f6b4a493afcd31d837a39664d6249e9be210f58d
Mentor <mentor@palokaj.co>
2022-11-01 20:27:05 +0100

    🐛 force lowercase as tracking format

--- functions/modules/services/airdrop_score.js
+++ functions/modules/services/airdrop_score.js
@@ -9,11 +9,14 @@ exports.register_potential_airdrop_usage = async ( data, context ) => {
         throw_if_invalid_context( context )
 
         // Get data
-        const { address, airdrop_tag } = data
+        let { address, airdrop_tag } = data
         if( !address || !airdrop_tag ) throw new Error( `Faulty tracking request` )
 
+        // Normalisation
+        address = address.toLowerCase()
+
         // Save data
-        await db.collection( `usage_tracking` ).doc( `${ address }`.toLowerCase() ).set( {
+        await db.collection( `usage_tracking` ).doc( address ).set( {
             [airdrop_tag]: increment( 1 ),
             updated: Date.now(),
             updated_human: new Date().toString()
