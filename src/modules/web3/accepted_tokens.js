d36098d1b99ac2fd4972ea48ca312c1db8b987e6
Mentor <mentor@palokaj.co>
2022-11-07 12:00:53 +0100

    🐛 fix max scale issue for rainbow

--- src/modules/web3/accepted_tokens.js
+++ src/modules/web3/accepted_tokens.js
@@ -3,10 +3,19 @@ import { dev } from "../helpers";
 
 export const tokens = [
 
+    // Native ETH
     {
         symbol: 'ETH',
-        address: 'native',
         chain_ids: dev ? [ 1, 5, 42161 ] : [ 1, 42161 ]
-    }
+    },
+
+    // USDC
+    // {
+    //     symbol: 'USDC',
+    //     address: {
+    //         1: `0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48`
+    //     },
+    //     chain_ids: [ 1 ]
+    // }
 
 ]
